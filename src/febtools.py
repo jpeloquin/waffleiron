25eed7867a169eeb3e40115d5f2a5bf316811325
John Peloquin <john.peloquin@gmail.com>
2013-02-05 18:06:27 -0500

    Taught Xpltreader to read solution steps and mesh.

--- src/febtools.py
+++ src/febtools.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import struct
+import numpy as np
 
 class Mesh:
     """Stores a mesh geometry."""
@@ -23,13 +24,20 @@ class Mesh:
 class MeshSolution(Mesh):
     """Stores a mesh and its FEA solution."""
 
+    node = []
+    element = []
+    step = []
+
     def __init__(self):
         # Set up blank Xplt
-        self.step = []
+        pass
         
     def readxplt(f):
         # Read data from .xplt file into Xplt
-        fdata = open(f,'rb').read()
+        reader = Xpltreader(f)
+        self.node, self.element = reader.mesh()
+        self.step = reader.solution()
+        time,
         
 class Xpltreader:
     """Assists in reading an FEBio xplt file."""
@@ -84,6 +92,24 @@ class Xpltreader:
         'surface_data':    33686784   # 0x02020500
         }
     tag2id = dict((v, k) for k, v in id2tag.items())
+    tag2elem_type = {
+        0: 'hex8',
+        1: 'penta6',
+        2: 'tet4',
+        3: 'quad4',
+        4: 'tri3',
+        5: 'truss2'
+        }
+    tag2item_type = {
+        0: 'float',
+        1: 'vec3f',
+        2: 'mat3fs'
+        }
+    tag2item_format = {
+        0: 'node',
+        1: 'item',
+        2: 'mult'
+        }
 
     def __init__(self, fpath):
         """Read xplt data from file"""
@@ -104,10 +130,107 @@ class Xpltreader:
                 raise Exception("The first 4 bytes of " + fpath + " "
                                 "do not match the FEBio spec: "
                                 "it is not a valid .feb file.")
-            # parse the file
-            self._parseblock(len(self.fdata))
         finally:
             f.close()
+    
+
+    def mesh(self):
+        """Reads node and element lists"""
+        # elem_type = self.tag2elem_type[
+        #     struct.unpack(self.endian + 'I', 
+        #                   self._lblock('root/geometry/domain_section/'
+        #                                'domain/domain_header/'
+        #                                'elem_type')[0])[0]]
+        # nnodes = int(elem_type[-1])
+        element = [struct.unpack('I' * (len(s) / 4), s) for s in                
+                   self._lblock('root/geometry/domain_section/domain/'
+                                'element_list/element')]
+        s = self._lblock('root/geometry/'
+                         'node_section/node_coords')[0]
+        node = []
+        for i in range(0, len(s), 12):
+            node.append(struct.unpack('f' * 3, s[i:i+12]))
+        return node, element
+
+    def solution(self):
+        # Read dictionary into `var`, a dictionary mapping strings to
+        # lists of tuples (type, format, name)
+        def rdict(name):
+            dict = 'root/dictionary/' + name + '_var/dict_item'
+            def trim(s): 
+                return s[0:s.find('\x00')]
+            if self._lblock(dict + '/item_type'):
+                typ = [self.tag2item_type[self._dword(s)] for s 
+                        in self._lblock(dict + '/item_type')]
+                fmt = [self.tag2item_format[self._dword(s)] for s 
+                          in self._lblock(dict + '/item_format')]
+                name = [trim(s) for s 
+                        in self._lblock(dict + '/item_name')]
+                return zip(typ, fmt, name)
+            else:
+                return None
+        var = {}
+        var['node'] = rdict('nodeset')
+        var['domain'] = rdict('domain')
+        var['surface'] = rdict('surface')
+        # Read solution steps into `step`, a list of dictionaries with
+        # keys = data names and values = lists of
+        # floats/vectors/tensors
+        time = [struct.unpack(self.endian + 'f', s)[0] for s in
+                self._lblock('state_section/state_header/time')]
+        def numerify(s, typ):
+            if len(s) == 0:
+                raise Exception('Input data has zero length.')
+            # Discard the 8 junk bytes
+            s = s[8:]
+            if typ == 'float':
+                v = struct.unpack(self.endian + 'f' * (len(s) / 4), s)
+            elif typ == 'vec3f':
+                if len(s) % 12 != 0:
+                    raise Exception('Input data cannot be evenly divided '
+                                    'into vectors.')
+                v = []
+                for i in range(0, len(s), 12):
+                    v.append(np.array(
+                            struct.unpack(self.endian + 'f' * 3,
+                                          s[i:i+12])))
+            elif typ == 'mat3fs':
+                v = []
+                if len(s) % 24 != 0:
+                    raise Exception('Input data cannot be evenly divided '
+                                    'into tensors.')
+                for i in range(0, len(s), 24):
+                    a = struct.unpack(self.endian + 'f' * 6, s[i:i+24])
+                    # The FEBio database spec does not document the
+                    # tensor order, but this is correct.
+                    v.append(np.array([[a[0], a[3], a[5]],
+                                       [a[3], a[1], a[4]],
+                                       [a[5], a[4], a[2]]]))
+            else:
+                raise Exception('Type ' + typ + ' not recognized.')
+            v = tuple(v)
+            return v
+        nsteps = len(time)
+        step = []
+        for istep in range(0, nsteps):
+            print('Step ' + str(istep) + ':')
+            this_step = {}
+            for k, v in var.iteritems():
+                if v:
+                    path = ('state_section/state_data/' + k + '_data'
+                            '/state_var/variable_data')
+                    data = self._lblock(path)
+                    for i in range(0, len(v)):
+                        s = data[istep * len(v) + i]
+                        typ = v[i][0]
+                        fmt = v[i][1]
+                        name = v[i][2]
+                        print('Found data named "' + name + '" in '
+                              + k + ' section.')
+                        this_step[name] = numerify(s, typ)
+            this_step['time'] = time[istep]
+            step.append(this_step)
+        return step
 
     def _lblock(self, pathstr, start = 4, end = None):
         """List data from block(s).
@@ -135,7 +258,6 @@ class Xpltreader:
         self.cursor = start
         while self.cursor < end:
             name, data = self._readblock()
-            print('Read ' + str(name))
             if name == blockpath[0]:
                 s = self.cursor + 8
                 e = s + len(data)
@@ -146,8 +268,19 @@ class Xpltreader:
                 result = result + data
                 self.cursor = e
             else:
-                self._mvcursor(8 + len(data))
+                self.cursor = self.cursor + 8 + len(data)
         return result
+
+    def _fblock(self, pathstr, start = 4, end = None):
+        """Returns data from first occurrence of block."""
+        block = self._lblock(pathstr, start, end)
+        if len(block) > 1:
+            raise Exception('Found multiple matches to ' 
+                            + pathstr + '; only expected one.')
+        if block:
+            return block[0]
+        else:
+            return None
 
     def _readblock(self):
         """Reads a block starting at the current cursor location."""
@@ -173,9 +306,12 @@ class Xpltreader:
         elif self.cursor > len(self.fdata):
             raise Exception('Tried to move cursor after end of file.')
 
-    def _dword(self):
-        dword = struct.unpack(self.endian+'I',
-                              self.fdata[self.cursor:self.cursor+4]
-                              )[0]
-        self.cursor = self.cursor + 4
+    def _dword(self, s = None):
+        if s:
+            dword = struct.unpack(self.endian + 'I', s)[0]
+        else:
+            dword = struct.unpack(self.endian+'I',
+                                  self.fdata[self.cursor:self.cursor+4]
+                                  )[0]
+            self.cursor = self.cursor + 4
         return dword
