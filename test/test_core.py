6f6c1bec71f02f731a54fb0e33634a0337659078
John Peloquin <john.peloquin@gmail.com>
2014-08-27 13:40:05 -0400

    Test case to exhibit bug in element_containing_point
    Currently, elements close to a given point P are chosen by finding
    the node closest to P, taking the connected elements, and testing
    each for if P is inside.
    
    However, the node closest to P is not necessarily part of the
    element enclosing P.

--- test/test_core.py
+++ test/test_core.py
@@ -29,9 +29,7 @@ class ElementContainingPointCubeHex8(unittest.TestCase):
     """
     def setUp(self):
         # The cube is bounded by -1 ≤ x ≤ 1, -1 ≤ y ≤ 1, and 0 ≤ z ≤ 2
-        self.soln = feb.input.XpltReader(os.path.join('test', 'fixtures', 'uniax-8cube.xplt'))
         self.model = feb.input.FebReader(os.path.join('test', 'fixtures', 'uniax-8cube.feb')).model()
-        self.model.apply_solution(self.soln)
 
     def test_outside(self):
         points = [(3, 3, 3),
@@ -47,7 +45,48 @@ class ElementContainingPointCubeHex8(unittest.TestCase):
         e = self.model.mesh.element_containing_point((-0.5, 0.5, 0.5))
         assert(e is self.model.mesh.elements[2])
 
+
+class ElementContainingPointNarrowParallelogram(unittest.TestCase):
+    """Test point in element for narrow parallelograms.
+
+    This test addresses the bug in which the elements which might
+    contain point P were selected by finding the node N closest to P
+    and checking if P was in any element connected to N.  However, N
+    is not necessarily part of the element that contains P.
+
+    """
+    def setUp(self):
+        nodes = [(0 , 0, 0),
+                 (1,  0, 0),
+                 (1, 10, 0),
+                 (0, 10, 0),
+                 (2,  5, 0),
+                 (2, 15, 0),
+                 (0,  0, 1),
+                 (1,  0, 1),
+                 (1, 10, 1),
+                 (0, 10, 1),
+                 (2,  5, 1),
+                 (2, 15, 1)]
+        # a rectangle (on left)
+        self.e1 = feb.element.Hex8.from_ids([0, 1, 2, 3, 6, 7, 8, 9], nodes)
+        # a parallelogram (on right)
+        self.e2 = feb.element.Hex8.from_ids([2, 4, 5, 3, 8, 10, 11, 9], nodes)
+        self.mesh = feb.Mesh(nodes, [self.e1, self.e2])
+
+    def test_point_in_rectangle(self):
+        """Test points that are nearest e2 nodes, but in e1.
+
+        """
+        points = [(0.5, 0.5, 0.5),
+                  (0.9, 5, 0.5)]
+        for p in points:
+            e = self.mesh.element_containing_point(p)
+            assert e is self.e1
+
+
 class ElementContainingPointQuad4(unittest.TestCase):
+
     """Test looking up with element contains a point (x, y, z)
 
     """
