ba10cc21738f265b8d3ff16d7a8262a37ebeb561
John Peloquin <john.peloquin@gmail.com>
2015-12-02 17:53:33 -0500

    Make imports python3-compatible

--- __init__.py
+++ __init__.py
@@ -1,12 +1,2 @@
-from core import Mesh, Model, Sequence, _canonical_face
-import analysis
-import compare
-import element
-import exceptions
-import geometry
-import input
-import material
-import mesh
-import output
-import plotting
-import selection
+from .core import Mesh, Model, Sequence, _canonical_face
+from . import compare, element, exceptions, geometry, input, material, mesh, output, plotting, selection
