4e930587ba27fc384b9640b379b2569142533903
John Peloquin <john.peloquin@gmail.com>
2016-03-07 20:24:17 -0500

    Import load_model into base namespace
    `load_model` is a major convenient function, so it should be available
    in the base namespace.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .core import Mesh, Model, Sequence, _canonical_face
+from .input import load_model
 from . import compare, element, exceptions, geometry, input, material, mesh, output, plotting, selection
