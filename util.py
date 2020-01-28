jpeloquin/waffleiron

commit 85b39d7afa225e828ee3a97b52964125d4dafc4a

Calculate floating point tolerance from target value

util.py
@@ -90,9 +90,12 @@ def apply_uniax_stretch(model, stretches, axis='x1'):
     model.steps[0]['control']['time stepper']['dtmax'] = seq_must
 
 
-def find_closest_timestep(target, times, steps, rtol=0.01, atol=1e-6):
+def find_closest_timestep(target, times, steps, rtol=0.01, atol="auto"):
     """Return step index closest to given time."""
     times = np.array(times)
+    if atol == "auto":
+        atol = max(np.nextafter(target, 0),
+                   np.nextafter(target, target**2))
     if len(steps) != len(times):
         raise ValueError("len(steps) ≠ len(times).  All steps must have a corresponding time value and vice versa.")
     idx = np.argmin(np.abs(times - target))

commit 6ab3aefd3ff3f5d976239e4bab3f9446e35b3d54

Fix automatic atol for timestep search
Tests were not passing, 85b39d7 broke find_closest_timestep.

util.py
@@ -94,8 +94,8 @@ def find_closest_timestep(target, times, steps, rtol=0.01, atol="auto"):
     """Return step index closest to given time."""
     times = np.array(times)
     if atol == "auto":
-        atol = max(np.nextafter(target, 0),
-                   np.nextafter(target, target**2))
+        atol = max(abs(np.nextafter(target, 0) - target),
+                   abs(np.nextafter(target, target**2) - target))
     if len(steps) != len(times):
         raise ValueError("len(steps) ≠ len(times).  All steps must have a corresponding time value and vice versa.")
     idx = np.argmin(np.abs(times - target))