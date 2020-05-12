74b1c3d8f66c52bba00fe9a4bf5dbd2fede5d634
John Peloquin <john.peloquin@gmail.com>
2020-05-12 19:45:38 -0400

    Add ScaledSequence to root namespace

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .model import Model, Mesh
-from .core import Body, ImplicitBody, ContactConstraint, Sequence, _canonical_face, _DEFAULT_TOL, NodeSet, FaceSet, ElementSet
+from .core import Body, ImplicitBody, ContactConstraint, Sequence, ScaledSequence, _canonical_face, _DEFAULT_TOL, NodeSet, FaceSet, ElementSet
 from .input import load_model
 from . import analysis, compare, load, control, element, exceptions, febio, geometry, input, material, mesh, output, plot, select, xplt
