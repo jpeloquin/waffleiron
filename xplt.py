4cefefa61e1f0fc6a04328bc71cd59eb0f610cf3
John Peloquin <john.peloquin@gmail.com>
2023-05-17 22:37:43 -0400

    Add mesh path for FEBio 4.1 plotfile

--- xplt.py
+++ xplt.py
@@ -194,7 +194,14 @@ tags_table = {
 # TODO: Version 49 changed rigid body force data, again.
 SUPPORTED_XPLT_VERSIONS = {2, 5, 48}
 # map of xplt version → path for mesh (2.0) or geometry (1.0) block
-MESH_PATH = {2: "root/mesh", 4: "root/mesh", 5: "root/mesh", 48: "mesh", 49: "mesh"}
+MESH_PATH = {
+    2: "root/mesh",
+    4: "root/mesh",
+    5: "root/mesh",
+    48: "mesh",
+    49: "mesh",
+    50: "mesh",
+}
 # map of xplt version → path for parts (2.0) or materials (1.0) block
 PARTS_PATH = {
     2: "root/materials",
