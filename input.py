jpeloquin/waffleiron

commit 7cabefcfa18e848b82da110a211b246c068f05da

Fix exception message

input.py
@@ -1,5 +1,6 @@
 import dataclasses
 import os
+from multiprocessing.managers import Value
 from pathlib import Path
 from typing import Any, List, Union, Dict, Tuple
 import warnings
@@ -271,7 +272,7 @@ def load_model(fpath, read_xplt=True, fallback_to_xplt=False):
         )
     # Attempt to read the FEBio xml file
     try:
-        model = FebReader(str(fp_feb)).model()
+        model = FebReader(fp_feb).model()
         feb_ok = True
     except UnsupportedFormatError as err:
         # The .feb file is some unsupported version
@@ -434,17 +435,15 @@ class FebReader:
 
     def __init__(self, file):
         """Read a file path as an FEBio xml file."""
-        self.file = file
+        self.file = str(file)
         self.root = normalize_xml(etree.parse(self.file).getroot())
         # Remove comments so iteration over child elements doesn't get
         # tripped up
         etree.strip_tags(self.root, etree.Comment)
         self.feb_version = self.root.attrib["version"]
         if self.root.tag != "febio_spec":
-            raise Exception(
-                "Root node is not 'febio_spec': '"
-                + file.name
-                + "is not an FEBio xml file."
+            raise ValueError(
+                f"Root node is not 'febio_spec': {file} is not an FEBio XML file."
             )
         if self.feb_version not in SUPPORTED_FEBIO_XML_VERS:
             msg = f"FEBio XML version {self.feb_version} is not supported by waffleiron"

commit 2383e2cd3899529ace08d571c325ec8969fa7b0d

Make FebReader accept file IO objects again
7cabefc broke this functionality

input.py
@@ -432,21 +432,20 @@ def textdata_table(fpath, delim=" "):
 class FebReader:
     """Read an FEBio XML file."""
 
-    def __init__(self, file):
+    def __init__(self, file_):
         """Read a file path as an FEBio xml file."""
-        self.file = str(file)
-        self.root = normalize_xml(etree.parse(self.file).getroot())
+        self.root = normalize_xml(etree.parse(file_).getroot())
         # Remove comments so iteration over child elements doesn't get
         # tripped up
         etree.strip_tags(self.root, etree.Comment)
         self.feb_version = self.root.attrib["version"]
         if self.root.tag != "febio_spec":
             raise ValueError(
-                f"Root node is not 'febio_spec': {file} is not an FEBio XML file."
+                f"Root node is not 'febio_spec': {file_} is not an FEBio XML file."
             )
         if self.feb_version not in SUPPORTED_FEBIO_XML_VERS:
             msg = f"FEBio XML version {self.feb_version} is not supported by waffleiron"
-            raise UnsupportedFormatError(msg, file, self.feb_version)
+            raise UnsupportedFormatError(msg, pth, self.feb_version)
         # Get the correct FEBio XML module
         self.febioxml_module = {
             "2.0": febioxml_2_0,